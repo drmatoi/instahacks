f3d8548f4cb5c8ef293717fab0428a59678b59d0
m4t01 <128631144+drmatoi@users.noreply.github.com>
2023-05-04 01:36:14 +0200

    Update insta-hack.sh

--- insta-hack.sh
+++ insta-hack.sh
@@ -10,10 +10,9 @@ Purple="\033[1;35m"      # Purple
 Cyan="\033[1;36m"        # Cyan
 White="\033[1;37m"       # White
 
-echo -e "$Purple Please wait 20 seconds and read this Info  "
+echo "Purple Please wait 20 seconds and read this Info"
 echo "[En] This tool does not work 100% as it should at the moment, use it at your own risk. I'm working on getting everything working again. Yours sincerely, M4T01\e[1;34m" 
-echo -e "$Red [De] Im Moment funktioniert dieses Tool nicht zu 100%, ich arbeite daran es wieder funktionsfähig zu machen, danke für das Verständnis!  LG M4T01 \e[1;31m"
-echo
+echo "[De] Im Moment funktioniert dieses Tool nicht zu 100%, ich arbeite daran es wieder funktionsfähig zu machen, danke für das Verständnis! LG M4T01"
 sleep 25 
 clear
 echo "Thanks!Have Fun!"
@@ -24,7 +23,7 @@ apt-get upgrade
 apt-get install python
 apt-get install python2
 clear
-echo -e "$Purple updated by m4t01 - Join my telegram t.me/moddingtool \e[1;34m"
+echo -e "$Purple Collection by m4t01 - Join my telegram t.me/moddingtool \e[1;34m"
        echo "Instagram : @wecrywiki"
 sleep 2.0
     echo -e " \e[96m  
@@ -40,21 +39,22 @@ echo " "
 figlet -f Bloody WeCry x PacMan Legion  | lolcat
 
     echo " "
-    echo -e "$Red                                 ⫸ Coded by$Yellow faizan$Red ⫷\033[0m"
+    echo -e "$Red                                 ⫸ Based on$Yellow faizan's code$Red ⫷\033[0m"
     echo -e "$Red                               ⫸$Purple WeCry x PacMan Legion $Red ⫷\033[0m"
-    echo -e "$Red                                 ⫸ Updated by $Yellow m4t01$Red ⫷\033[0m"
+    echo -e "$Red                                 ⫸ Collection by $Yellow m4t01$Red ⫷\033[0m"
 echo " "
 echo -e " $Green     |---------------------------------------------------------------------|"
-echo -e " $Green     ||----------------------------$Cyan [Tools] $Green---------------------------||"
+echo -e " $Green     ||----------------------------$Cyan [Tools/Collection] $Green---------------------------||"
 echo -e " $Green     ||                                                                   ||"
-echo -e " $Green     ||             $Purple==>$Yellow[1️⃣] Insta information gathering$Green                    ||"
-echo -e " $Green     ||             $Purple==>️$Yellow[2️⃣] Insta brute force attack$Green                       ||"
-echo -e " $Green     ||             $Purple==>$Yellow[3️⃣] Insta auto reporter$Green                            ||"
-echo -e " $Green     ||             $Purple==>$Yellow[4️⃣] Insta phishing page(coming soon)$Green               ||"
-echo -e " $Green     ||             $Purple==>$Yellow[5️⃣] Telegram Links$Green                                  ||"
-echo -e " $Green     ||             $Purple==>$Yellow[6️⃣] Remove scripts$Green                                  ||"
-echo -e " $Green     ||             $Purple==>$Yellow[7️⃣] About$Green                                          ||"
-echo -e " $Green     ||             $Purple==>$Yellow[8️⃣] exit$Green                                           ||"
+echo -e " $Green     ||             $Purple==>$Yellow[1] Insta information gathering$Green                    ||"
+echo -e " $Green     ||             $Purple==>️$Yellow[2] Insta brute force attack$Green                       ||"
+echo -e " $Green     ||             $Purple==>$Yellow[3] Insta auto reporter$Green                            ||"
+echo -e " $Green     ||             $Purple==>$Yellow[4] Insta phishing page(coming soon)$Green               ||"
+echo -e " $Green     ||             $Purple==>$Yellow[5] Telegram Links$Green                                  ||"
+echo -e " $Green     ||             $Purple==>$Yellow[6] Remove scripts$Green                                  ||"
+echo -e " $Green     ||             $Purple==>$Yellow[7] About$Green                                          ||"
+echo -e " $Green     ||             $Purple==>$Yellow[8] exit$Green                                           ||"
+echo -e " $Green     ||             $Purple==>$Yellow[999] Reset/Update Tool$Green                                           ||"
 echo -e " $Green     ||                                                                   ||"                                                                                       
 echo -e " $Green     ||---------------------------$Cyan [select option] $Green-----------------------||"
 echo -e " $Green     |---------------------------------------------------------------------|"
@@ -91,10 +91,11 @@ echo " "
     elif [ $ch -eq 5 ];then
         echo -e "\e[1;34m Telegram: t.me/moddingtool"
 echo -e "\e[1;34m Telegram: t.me/mrmatoi"
+echo -e "\e[1;34m Libary: t.me/wecrywiki"
         cd $HOME
 
         exit
-   elif [ $ch -eq reset ];then
+   elif [ $ch -eq 999];then
 rm -rf insta-hack
         git clone https://github.com/drmatoi/instahacks
         cd insta-hack
