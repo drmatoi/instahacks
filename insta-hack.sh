8618150b8e209e2eff9584819fa6aa79c938aa44
m4t01 <128631144+drmatoi@users.noreply.github.com>
2023-05-04 02:07:44 +0200

    Update insta-hack.sh

--- insta-hack.sh
+++ insta-hack.sh
@@ -10,8 +10,9 @@ Purple="\033[1;35m"      # Purple
 Cyan="\033[1;36m"        # Cyan
 White="\033[1;37m"       # White
 
-echo -e "Thanks for using m4t01's collection!"
-sleep 5
+echo -e "$Red Thanks for using m4t01's collection!\e[1;31m"
+echo -e "$Yellow This tool still has some bugs, I'm always trying to fix them please be patient. Your Dev :) \e[1;33m"
+sleep 6
 clear
 apt-get update
 apt-get upgrade
