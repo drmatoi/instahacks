ac8b337aa14a51c19f46f8659c2433e6ec0e2410
m4t01 <128631144+drmatoi@users.noreply.github.com>
2023-05-04 02:21:27 +0200

    Update insta-hack.sh

--- insta-hack.sh
+++ insta-hack.sh
@@ -104,7 +104,76 @@ echo -e "\e[1;34m Libary: t.me/wecrywiki"
         
         exit
    elif [ $ch -eq 7 ];then
-        echo -e 
+        echo -e "$Cyan This Is an all in one hacking tool/collection for Instagram and other Social Medias like Youtube,WhatsApp & Co. The complete collection layout is based on https://github.com/IncredibleHacker/insta-hack. This Tool also links to other GitHub Users. Full Credits to them :) ,
+
+Features:
+Insta information gathering
+Insta brute force attack
+Insta auto repoter
+Remove script
+WeCry Telegram List
+---------------
+Requirements
+Data connection
+Internet 200MB
+storage 400MB
+No Root
+----------------
+Available On :
+Termux (tested on 04.05.23)
+Kali Linux
+Tested On:
+TermuxMi Note 9 pro
+Samsung Galaxy A22
+Virtual Box 7.0
+----------------
+
+INSTALLATION [Termux]
+---------
+apt update
+apt upgrade
+pkg install python
+pkg install python2
+pkg install git
+git clone https://github.com/drmatoi/instahacks
+ls
+cd instahacks
+pip3 install -r requirements.txt
+chmod +x *
+bash setup.sh
+bash insta-hack.sh
+--------------
+INSTALLATION [Kali Linux]
+sudo apt install python
+sudo apt install python2
+sudo apt install git
+git clone https://github.com/drmatoi/instahacks
+ls
+cd instahacks
+pip3 install -r requirements.txt
+chmod +x *
+sudo bash insta-hack.sh
+--------------------------------
+Collection by m4t01 / Mr.Matoi:
+--------------------------------
+
+Need to Update/Reset the Tool.
+
+type
+
+rm -rf instahacks
+git clone https://github.com/drmatoi/instahackscd instahacks
+bash insta-hack.sh
+
+I will add this to the bash script soon!
+-----------------------------------------
+
+Warning:This tool is only for educational purpose. If you use this tool for other purposes except education we will not be responsible in such cases.
+-----------------------------------------
+
+Socials:
+Instagram: @wecrywiki
+ Telegram: @mrmatoi \e[1;36m"
         cd $HOME
 
         exit
