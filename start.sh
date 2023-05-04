f751b00c825a2c316720d99c787a8d085c9d47d5
m4t01 <128631144+drmatoi@users.noreply.github.com>
2023-05-04 07:24:52 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# colour  
+ Black="\033[1;30m"       # Black 
+ Red="\033[1;31m"         # Red 
+ Green="\033[1;32m"       # Green 
+ Yellow="\033[1;33m"      # Yellow 
+ Blue="\033[1;34m"        # Blue 
+ Purple="\033[1;35m"      # Purple 
+ Cyan="\033[1;36m"        # Cyan 
+ White="\033[1;37m"       # White
