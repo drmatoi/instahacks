990c45a49e8004b47f7a587a9f31538963ae4bbd
m4t01 <128631144+drmatoi@users.noreply.github.com>
2023-05-04 08:27:59 +0200

    Update venom.sh

--- venom.sh
+++ venom.sh
@@ -1,1 +1,27 @@
 
+#!/usr/bin/bash 
+ clear 
+ figlet -c "m4t01" 
+ echo " " 
+ read -p "Do you want to continue installing setup? [y/n] : " answer 
+ case $answer in 
+         y) 
+                 clear 
+                 figlet -c "Team Venom" 
+                 apt-get update 
+                 apt-get upgrade 
+                 apt-get install figlet 
+                 apt-get install git 
+                 bash venom.sh 
+                 ;; 
+         n) 
+                 echo " " 
+                 echo -e "\e[1;31m Aborting the installation.... \e[0m" 
+                 exit 
+                 ;; 
+         *) 
+                 echo " " 
+                 echo "I don't understand you" 
+                 exit 
+                 ;; 
+ esac
